d00b90a49be9cf70f1de53512ab034a4523ae970
Schnield <schnield416@gmail.com>
2026-04-20 06:30:53 +0700

    Menambahkan animasi hover dan tekan pada aircraftCard

--- lib/widgets/aircraft_card.dart
+++ lib/widgets/aircraft_card.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import '../models/aircraft.dart';
 
-class AircraftCard extends StatelessWidget {
+class AircraftCard extends StatefulWidget {
   final Aircraft? aircraft;
   final bool isLoading; // Jika true, tampilkan Skeleton
   final VoidCallback? onTap;
@@ -14,16 +14,54 @@ class AircraftCard extends StatelessWidget {
   });
 
   @override
+  State<AircraftCard> createState() => _AircraftCardState();
+}
+
+class _AircraftCardState extends State<AircraftCard> {
+  bool _isActive = false; // State untuk mengontrol tampilan skeleton saat mengetik
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      padding: const EdgeInsets.all(12),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(16),
-        boxShadow: [BoxShadow(color: Colors.black.withValues(alpha: 0.05), blurRadius: 10)],
+    if (widget.isLoading) {
+      return _buildBaseContainer(child: _buildSkeleton());
+    }
+
+    return MouseRegion(
+      onEnter: (_) => setState(() => _isActive = true),
+      onExit: (_) => setState(() => _isActive = false),
+      child: GestureDetector(
+        onTapDown: (_) => setState(() => _isActive = true),
+        onTapUp: (_) => setState(() => _isActive = false),
+        onTapCancel: () => setState(() => _isActive = false),
+        onTap: widget.onTap,
+        child: AnimatedScale(
+          scale: _isActive ? 1.05 : 1.0, // Efek zoom saat hover/tap aktif 1.05 atau normal 1.0/false
+          duration: const Duration(milliseconds: 200), // Durasi animasi zoom
+          child: _buildBaseContainer (
+            isActive : _isActive, // Kirim state isActive ke _buildBaseContainer untuk mengubah warna saat hover/tap
+            child: _buildContent(), // Konten utama card (gambar, nama, asal)
+          ),
+        ),
       ),
-      child: isLoading ? _buildSkeleton() : _buildContent(),
+  );
+  }
+
+  Widget _buildBaseContainer({required Widget child, bool isActive = false}) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      decoration: BoxDecoration(
+        color: isActive ? Colors.indigo[50] : Colors.white, // Warna berubah saat hover/tap
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withValues(alpha: isActive ? 0.3 : 0.1), // Shadow lebih gelap saat hover/tap
+            blurRadius: isActive ? 20 : 10, // Shadow lebih besar saat hover/tap
+            offset: Offset(0, isActive ? 10 : 5), // Shadow lebih jauh saat hover/tap
+          ),
+        ],
+      ),
+      child: child,
     );
   }
 
@@ -31,7 +69,11 @@ class AircraftCard extends StatelessWidget {
   Widget _buildSkeleton() {
     return Row(
       children: [
-        Container(width: 100, height: 80, decoration: BoxDecoration(color: Colors.grey[300], borderRadius: BorderRadius.circular(12))),
+        Container(
+          width: 100, 
+          height: 80, 
+          decoration: BoxDecoration(color: Colors.grey[300], borderRadius: BorderRadius.circular(12))
+        ),
         const SizedBox(width: 16),
         Expanded(
           child: Column(
@@ -49,27 +91,27 @@ class AircraftCard extends StatelessWidget {
 
   // UI Card saat data sudah tersedia
   Widget _buildContent() {
-    return InkWell(
-      onTap: onTap,
-      child: Row(
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(12),
-            child: Image.asset(aircraft!.imagePath, width: 100, height: 80, fit: BoxFit.cover),
+    return Row(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(12),
+          child: Image.asset(widget.aircraft!.imagePath, width: 100, height: 80, fit: BoxFit.cover),
+        ),
+        const SizedBox(width: 16),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(widget.aircraft!.name, style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+              Text(widget.aircraft!.origin, style: TextStyle(color: Colors.grey[600])),
+            ],
           ),
-          const SizedBox(width: 16),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(aircraft!.name, style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                Text(aircraft!.origin, style: TextStyle(color: Colors.grey[600])),
-              ],
-            ),
-          ),
-          const Icon(Icons.chevron_right, color: Colors.grey),
-        ],
-      ),
+        ),
+        Icon(
+          Icons.chevron_right,
+          color: _isActive ? Colors.indigo : Colors.grey
+        ), // Ikon panah berubah warna saat hover/tap
+      ],
     );
   }
 }
